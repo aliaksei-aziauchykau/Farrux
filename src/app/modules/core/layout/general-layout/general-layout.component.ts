60a257ba72b3c6da3ee39d28d482cc9f80a54feb
MINSK\Aliaksei_Aziauchykau <azyavchikov.aleksey@gmail.com>
2019-07-25 18:40:29 +0300

    [AA] Impr

--- src/app/modules/core/layout/general-layout/general-layout.component.ts
+++ src/app/modules/core/layout/general-layout/general-layout.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-general-layout',
-  templateUrl: './general-layout.component.html',
-  styleUrls: ['./general-layout.component.less']
+    selector: 'app-general-layout',
+    templateUrl: './general-layout.component.html',
+    styleUrls: ['./general-layout.component.less']
 })
 export class GeneralLayoutComponent implements OnInit {
 
-  constructor() { }
+    public selectedValue: string;
+    public types: string;
+    public taxes: string;
+    public dateComming: Date;
+    public dateOutComming: Date;
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
